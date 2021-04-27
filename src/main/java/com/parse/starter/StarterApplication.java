4c13cf8d4e988d22a29740a1f0b2c1bb68c4f760
Alchemistgg <74482590+Alchemistgg@users.noreply.github.com>
2021-04-27 11:01:06 +0545

    Update StarterApplication.java

--- src/main/java/com/parse/starter/StarterApplication.java
+++ src/main/java/com/parse/starter/StarterApplication.java
@@ -1,11 +1,4 @@
-/*
- * Copyright (c) 2015-present, Parse, LLC.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
+
 package com.parse.starter;
 
 import android.app.Application;
@@ -38,7 +31,7 @@ public class StarterApplication extends Application {
 
     /*ParseObject object = new ParseObject("ExampleObject");
     object.put("myNumber", "1135");
-    object.put("myString", "agniva");
+    object.put("myString", "suryo");
 
     object.saveInBackground(new SaveCallback () {
       @Override
